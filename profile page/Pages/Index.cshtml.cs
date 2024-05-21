e25a37707c91bb97c4740279921ef42478cefddd
HP <HP@DESKTOP-GETFNK5>
2024-05-21 06:08:35 +0300

    Add project files.

---
+++ profile page/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace profile_page.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public void
+    }
+}
